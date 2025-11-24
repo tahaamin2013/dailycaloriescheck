8a7fb43be455ac1f47517b6553ef8729be18effd
tahaamin2013 <tahaamindob2013@gmail.com>
2025-11-24 18:36:16 +0500

    d

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,22 +5,9 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-
-  {
-    rules: {
-      "react/no-unescaped-entities": "off",
-      "@next/next/no-img-element": "off",
-      "@typescript-eslint/no-unused-vars": "off",
-      "no-console": "off",
-      "no-undef": "off",
-      // Sab rules off karne ke liye:
-      "@typescript-eslint/no-explicit-any": "off",
-      "no-unused-vars": "off",
-      "react-hooks/exhaustive-deps": "off",
-    },
-  },
-
+  // Override default ignores of eslint-config-next.
   globalIgnores([
+    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
